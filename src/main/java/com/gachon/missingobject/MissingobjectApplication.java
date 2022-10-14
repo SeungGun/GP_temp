42f21a99a83f94d7a0f4289074e6aca0cbf72c6d
SeungGun <saverrw2864@naver.com>
2022-10-14 14:16:52 +0900

    initialize setting files

---
+++ src/main/java/com/gachon/missingobject/MissingobjectApplication.java
@@ -0,0 +1,13 @@
+package com.gachon.missingobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissingobjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissingobjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gachon/missingobject/MissingobjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachon.missingobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissingobjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
