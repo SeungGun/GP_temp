9effeba8d81b8627d1fece123b992f57dca9fd5a
SeungGun <saverrw2864@naver.com>
2022-10-14 20:46:35 +0900

    chore: connect python command line with example

--- src/main/java/com/gachon/missingobject/controller/TempController.java
+++ src/main/java/com/gachon/missingobject/controller/TempController.java
@@ -1,14 +1,40 @@
 package com.gachon.missingobject.controller;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 public class TempController {
 
     @GetMapping(value="/hello")
-    public String hello(){
-        return "hello world";
+    public String hello() throws IOException {
+        System.out.println("Python call");
+
+        String[] command = new String[4];
+        command[0] = "python3";
+        command[1] = "test.py";
+        command[2] = "20";
+        command[3] = "40";
+        CommandLine commandLine = CommandLine.parse(command[0]);
+        for (String s : command) commandLine.addArgument(s);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(outputStream);
+        DefaultExecutor executor = new DefaultExecutor();
+
+        executor.setStreamHandler(pumpStreamHandler);
+
+        int result = executor.execute(commandLine);
+        System.out.println("result = " + result);
+        System.out.println("output = " + outputStream.toString());
+        return outputStream.toString();
     }
 
 }
