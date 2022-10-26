28324a52da59d2cf14c49ea25a46db5348cc892f
SeungGun <saverrw2864@naver.com>
2022-10-26 23:38:34 +0900

    test: argument test

--- src/main/java/com/gachon/missingobject/controller/TempController.java
+++ src/main/java/com/gachon/missingobject/controller/TempController.java
@@ -5,6 +5,7 @@ import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
@@ -14,14 +15,14 @@ import java.io.IOException;
 public class TempController {
 
     @GetMapping(value = "/hello")
-    public String hello() throws IOException {
+    public String hello(@RequestParam("value1") int a, @RequestParam("value2") int b) throws IOException {
         System.out.println("Python call");
 
         String[] command = new String[4];
         command[0] = "python3";
-        command[1] = "test.py";
-        command[2] = "20";
-        command[3] = "40";
+        command[1] = "test.py"; // 실행할 파일(Root 기준으로 상대경로 가져오기)
+        command[2] = a + ""; // argument1
+        command[3] = b + ""; // argument2
         CommandLine commandLine = CommandLine.parse(command[0]);
         for (int i = 1, n = command.length; i < n; ++i)
             commandLine.addArgument(command[i]);
@@ -33,8 +34,8 @@ public class TempController {
         executor.setStreamHandler(pumpStreamHandler);
 
         int result = executor.execute(commandLine);
-        System.out.println("result = " + result);
-        System.out.println("output = " + outputStream.toString());
+        System.out.println("result = " + result); // 실행 코드 (값이 0이면 성공)
+        System.out.println("output = " + outputStream.toString()); // 리턴 값
         return outputStream.toString();
     }
 
