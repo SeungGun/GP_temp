b1281cfdb9c3a862140fdc5dd0c0eb0460d4bc52
SeungGun <saverrw2864@naver.com>
2022-11-03 21:00:29 +0900

    test: remove numpy

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import sys
-import numpy as np
+# import numpy as np
 
 def sum(v1, v2):
     result = int(v1) + int(v2)
     print(result)
-    print(np.arange(10))
+#     print(np.arange(10))
 
 def main(argv):
     sum(argv[1], argv[2])
